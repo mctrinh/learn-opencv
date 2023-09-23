47cb0db76a927f5e3c9b56cd38a91064c75ba533
mctrinh <tmc.ksxd@gmail.com>
2023-09-23 12:57:48 +0900

    histogram backprojection

--- img_process_histogram.py
+++ img_process_histogram.py
@@ -163,9 +163,153 @@ plt.legend(('cdf','histogram'), loc = 'upper left')
 plt.show()
 
 
-# Histograms Equalization in OpenCV
+# Global Histograms Equalization in OpenCV
 img = cv.imread('./image/wiki.jpeg', cv.IMREAD_GRAYSCALE)
 assert img is not None, "file could not be read, check with os.path.exists()"
 equ = cv.equalizeHist(img)
 res = np.hstack((img,equ)) #stacking images side-by-side
-cv.imwrite('./output/res.png',res)
+cv.imwrite('./image/wiki-equalized.png',res)
+
+
+# CLAHE: Contrast Limited Adaptive Histogram Equalization
+# (tilesize=8x8, each tile is histogram equalized as usual)
+''' If there is noise, it will be amplified. Apply constrast limiting to avoid it.
+    If any histogram bin is above the specified contrast limit, default=40, those
+    pixels are clipped and distributed uniformly to other bins before applying
+    histogram equalization. After equalization, to remove artifacts in title borders,
+    bilinear interpolation is applied.
+'''
+
+img = cv.imread('./image/tomato.jpeg', cv.IMREAD_GRAYSCALE)
+assert img is not None, "file could not be read, check with os.path.exists()"
+
+''' Global histogram equalization'''
+equ = cv.equalizeHist(img)
+res = np.hstack((img,equ)) #stacking images side-by-side
+cv.imwrite('./image/tomato-equalized.png',res)
+
+# Create a CLAHE object (Arguments are optional).
+clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+cl1 = clahe.apply(img)
+cv.imwrite('./image/tomato-clahe.png',cl1)
+
+
+
+
+# ------------------------------ 2D Histograms -----------------------------
+"""
+1D histogram: 1 feature (grayscale intensity value of pixels), BGR -> grayscale.
+2D histogram: 2 features (hue & saturation values of pixels),  BGR -> HSV (color histogram)
+
+cv.calcHist(channels=[0,1],             : both hue and saturation plane
+            bins=[180,256],
+            range=[0,180,0,255])
+"""
+
+img = cv.imread('./image/home-1.jpeg')
+assert img is not None, "file could not be read, check with os.path.exists()"
+hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
+hist = cv.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256])
+
+# Plot a grayscale histogram
+cv.imshow("2D Histogram", hist)
+cv.waitKey(0)
+
+# Plot using matplotlib
+plt.imshow(hist, interpolation='nearest')
+plt.show()
+
+
+
+# ------------------------------ Histogram Backprojection -----------------------------
+"""
+Refs: https://docs.opencv.org/4.8.0/dc/df6/tutorial_py_histogram_backprojection.html
+
+What is it actually in simple words?
+It is used for image segmentation or finding objects of interest in an image.
+It creates an image of the same size (but single channel) as that of our input image,
+where each pixel corresponds to the probability of that pixel belonging to our object.
+The output image will have our object of interest in more white compared to remaining part.
+Histogram Backprojection is used with camshift algorithm etc.
+
+How do we do it?
+We create a histogram of an image containing our object of interest (in our case, the ground,
+leaving player and other things). The object should fill the image as far as possible for
+better results. And a color histogram is preferred over grayscale histogram, because
+color of the object is a better way to define the object than its grayscale intensity.
+We then "back-project" this histogram over our test image where we need to find the object,
+we calculate the probability of every pixel belonging to the ground and show it.
+The resulting output on proper thresholding gives us the ground alone.
+"""
+
+
+# ------  Numpy ------
+# Roi is the object or region of object we need to find
+img = cv.imread('./image/messi.jpg')
+assert img is not None, "file could not be read, check with os.path.exists()"
+roi = img[380:460, 50:130]
+hsv = cv.cvtColor(roi,cv.COLOR_BGR2HSV)
+
+# Target is the image we search in
+target = cv.imread('./image/messi.jpg')
+assert target is not None, "file could not be read, check with os.path.exists()"
+hsvt = cv.cvtColor(target,cv.COLOR_BGR2HSV)
+
+# Find the histograms using calcHist. Can be done with np.histogram2d also
+M = cv.calcHist([hsv],[0, 1], None, [180, 256], [0, 180, 0, 256])
+I = cv.calcHist([hsvt],[0, 1], None, [180, 256], [0, 180, 0, 256])
+
+'''
+Ratio R=M/I, then backproject R, use R as palette and create a new image with every pixel
+as its corresponding probability of being target B(x,y) = R[h(x,y), s(x,y)], where h is
+hue and s is saturation of the pixel at (x,y).
+Then apply the condition B(x,y) = min[B(x,y),1]
+'''
+R = np.divide(M,I,out=np.zeros_like(M),where=I!=0)
+h,s,v = cv.split(hsvt)
+B = R[h.ravel(),s.ravel()]
+B = np.minimum(B,1)
+B = B.reshape(hsvt.shape[:2])
+
+'''
+Now apply a convolution with a circular disc, B = D*B, where D is the disc kernel
+'''
+disc = cv.getStructuringElement(cv.MORPH_ELLIPSE,(5,5))
+cv.filter2D(B,-1,disc,B)
+B = np.uint8(B)
+cv.normalize(B,B,0,255,cv.NORM_MINMAX)
+
+'''
+Now the location of maximum intensity gives us the location of object.
+If we expect a region in the image, thresholding for a suitable value gives a nice result.
+'''
+ret,thresh = cv.threshold(B,50,255,0)
+
+
+# ------ Backprojection in OpenCV ------
+img = cv.imread('./image/messi.jpg')
+assert img is not None, "file could not be read, check with os.path.exists()"
+roi = img[380:460, 50:130]
+hsv = cv.cvtColor(roi,cv.COLOR_BGR2HSV)
+
+target = cv.imread('./image/messi.jpg')
+assert target is not None, "file could not be read, check with os.path.exists()"
+hsvt = cv.cvtColor(target,cv.COLOR_BGR2HSV)
+
+# Calculating object histogram
+roihist = cv.calcHist([hsv],[0, 1], None, [180, 256], [0, 180, 0, 256] )
+
+# Normalize histogram and apply backprojection
+cv.normalize(roihist,roihist,0,255,cv.NORM_MINMAX)
+dst = cv.calcBackProject([hsvt],[0,1],roihist,[0,180,0,256],1)
+
+# Now convolute with circular disc
+disc = cv.getStructuringElement(cv.MORPH_ELLIPSE,(5,5))
+cv.filter2D(dst,-1,disc,dst)
+
+# Threshold and binary AND
+ret,thresh = cv.threshold(dst,50,255,0)
+thresh = cv.merge((thresh,thresh,thresh))
+res = cv.bitwise_and(target,thresh)
+res = np.vstack((target,thresh,res))
+cv.imwrite('./image/messi-backprojection.jpg',res)
